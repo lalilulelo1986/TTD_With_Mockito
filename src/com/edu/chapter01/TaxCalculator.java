ec5571ad2fd0824265aed435026bcaba7aecc126
lalilulelo1986 <m.kuzichev@ecc.kz>
2018-04-16 19:55:25 +0600

    first

---
+++ src/com/edu/chapter01/TaxCalculator.java
@@ -0,0 +1,12 @@
+package com.edu.chapter01;
+
+public class TaxCalculator {
+
+	public double calculate(double taxableIncome) {
+		if (taxableIncome > 500000) {
+			return 50000 + ((taxableIncome - 500000) / 5);
+		}
+		return (taxableIncome / 10);
+	}
+
+}
---
+++ test/com/edu/chapter01/TaxCalculatorTest.java
@@ -0,0 +1,32 @@
+package com.edu.chapter01;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TaxCalculatorTest {
+
+	@Test
+	public void when_income_less_than_5Lacs_then_deducts_10_percent_tax() {
+		TaxCalculator taxCalculator = new TaxCalculator();
+		double payableTax = taxCalculator.calculate(400000.00);
+		assertTrue(40000 == payableTax);
+
+		payableTax = taxCalculator.calculate(0);
+		assertTrue(0 == payableTax);
+
+		payableTax = taxCalculator.calculate(500000.00);
+		assertTrue(50000 == payableTax);
+	}
+
+	@Test
+	public void when_income_between_5lacs_and_10lacs_then_deducts_fifty_thousand_plus_20_percent_above_5lacs() {
+		TaxCalculator taxCalculator = new TaxCalculator();
+		double payableTax = taxCalculator.calculate(800000.00);
+		double expectedTaxForFirstFiveHundredThousand = 50000;
+		double expectedTaxForReminder = 60000;
+		double expectedTotalTax = expectedTaxForFirstFiveHundredThousand + expectedTaxForReminder;
+		assertTrue(expectedTotalTax == payableTax);
+	}
+
+}
